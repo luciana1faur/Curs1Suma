864c15984c080b1b02697d049a7f8e2ed2b0edc0
luciana1faur <luciana_faur@yahoo.com>
2020-09-21 05:29:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  x = 2;
+  y = 3;
+  double suma;
+  suma = x+y;
+  cout << "suma numerelor x si y este: " << suma << endl;
+  return 0;
+}
